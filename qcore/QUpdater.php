7e7316d868271305a845d03311003d7abf63478a
Andrew Quardex <megasounds@mail.ru>
2020-04-15 01:08:07 +0300

    no message

--- qcore/QUpdater.php
+++ qcore/QUpdater.php
@@ -4,4 +4,13 @@ namespace quarsintex\quartronic\qcore;
 
 class QUpdater {
 
+    protected $action;
+
+    static function run($rootDir='',$lockDir='') {
+        if (!$rootDir) $rootDir = __DIR__.'/../../../';
+        chdir($rootDir);
+        $input = new \Symfony\Component\Console\Input\ArrayInput(array('command' => 'update'));
+        $application = new \Composer\Console\Application();
+        $application->run($input);
+    }
 }
