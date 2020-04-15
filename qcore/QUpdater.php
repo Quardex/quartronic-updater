6beaf4886b5b88767b57f9d0c0ae0f3bab34b517
Andrew Quardex <megasounds@mail.ru>
2020-04-15 04:52:45 +0300

    no message

--- qcore/QUpdater.php
+++ qcore/QUpdater.php
@@ -6,11 +6,15 @@ class QUpdater {
 
     protected $action;
 
-    static function run($rootDir='',$lockDir='') {
-        if (!$rootDir) $rootDir = __DIR__.'/../../../../';
-        chdir($rootDir);
+    static function update() {
         $input = new \Symfony\Component\Console\Input\ArrayInput(array('command' => 'update'));
         $application = new \Composer\Console\Application();
         $application->run($input);
+    }
+
+    static function run($rootDir='') {
+        if (!$rootDir) $rootDir = __DIR__.'/../../../../';
+        chdir($rootDir);
+        echo shell_exec('php -r "require_once(\''.$rootDir.'/vendor/autoload.php\');\quarsintex\quartronic\qcore\QUpdater::update();"');
     }
 }
