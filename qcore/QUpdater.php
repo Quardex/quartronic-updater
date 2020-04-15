20abb3af7ee0fb2c2af374f943ce6f886a485d65
Andrew Quardex <megasounds@mail.ru>
2020-04-15 05:05:55 +0300

    no message

--- qcore/QUpdater.php
+++ qcore/QUpdater.php
@@ -15,6 +15,6 @@ class QUpdater {
     static function run($rootDir='') {
         if (!$rootDir) $rootDir = __DIR__.'/../../../../';
         chdir($rootDir);
-        echo shell_exec('php -r "require_once(\''.$rootDir.'/vendor/autoload.php\');\quarsintex\quartronic\qcore\QUpdater::update();"');
+        echo shell_exec('php -r "require_once(\'vendor/autoload.php\');\quarsintex\quartronic\qcore\QUpdater::update();"');
     }
 }
